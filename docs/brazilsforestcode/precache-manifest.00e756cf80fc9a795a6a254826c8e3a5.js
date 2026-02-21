1b0ad8e2a4f5d159c086bdf826a35fc8f155ac09
Heitor Carneiro <heitorgcarneiro@gmail.com>
2026-02-21 16:59:13 -0300

    feat: add docs

---
+++ docs/brazilsforestcode/precache-manifest.00e756cf80fc9a795a6a254826c8e3a5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2a6ebe9debccd2cd144568e525e392",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2bc501ef139da8dc328a",
+    "url": "./static/css/main.9e8d31ff.chunk.css"
+  },
+  {
+    "revision": "b2b64d918e55caf0fbeb",
+    "url": "./static/js/2.f73a0ef3.chunk.js"
+  },
+  {
+    "revision": "6211132799115a7b047d842c9563abc0",
+    "url": "./static/js/2.f73a0ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc501ef139da8dc328a",
+    "url": "./static/js/main.a1ad96fe.chunk.js"
+  },
+  {
+    "revision": "98f4ba116767e8c9348b",
+    "url": "./static/js/runtime-main.fd4a1bdc.js"
+  },
+  {
+    "revision": "d3e895f36e7198ef21b52b4bdfbd40ef",
+    "url": "./static/media/bmu-logo.d3e895f3.png"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "./static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "3f38353f956e950ee64bc458843fc035",
+    "url": "./static/media/restore-logo.3f38353f.png"
+  },
+  {
+    "revision": "121e17693489227885322697d68f1bf1",
+    "url": "./static/media/resume-en.121e1769.png"
+  },
+  {
+    "revision": "900a753361381592ae08fb4274790a85",
+    "url": "./static/media/resume-pt.900a7533.png"
+  }
+]);
---
+++ docs/luccme/precache-manifest.cad20a93eddf0fd65aa9a1c63762d893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87269c8c3812fde856537de174fbef0f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e20df22c24772bc097b9",
+    "url": "./static/css/main.6329b3a2.chunk.css"
+  },
+  {
+    "revision": "440bd2b67933693e2c32",
+    "url": "./static/js/2.40c82cae.chunk.js"
+  },
+  {
+    "revision": "6211132799115a7b047d842c9563abc0",
+    "url": "./static/js/2.40c82cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e20df22c24772bc097b9",
+    "url": "./static/js/main.60a0f061.chunk.js"
+  },
+  {
+    "revision": "43bcc005f80b78b7ce97",
+    "url": "./static/js/runtime-main.4d4c308f.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "./static/media/getFetch.013f0b90.cjs"
+  }
+]);
